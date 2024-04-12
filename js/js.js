e0740988d97ec426e9bd6f28c577abe21e7c0095
iCheckmate-git <123750808+iCheckmate-git@users.noreply.github.com>
2024-04-13 00:52:01 +0300

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -49,32 +49,14 @@ $('.reviews__slider, .header-hero__slider').owlCarousel({
     dots: false,
     // autoWidth: true
 });
+  const mobile_are = document.querySelectorAll('.mobile_are li');
 
-  // Initialize and add the map
-  // let map;
-
-  // async function initMap() {
-  //   // The location of Uluru
-  //   const position = { lat: -25.344, lng: 131.031 };
-  //   // Request needed libraries.
-  //   //@ts-ignore
-  //   const { Map } = await google.maps.importLibrary("maps");
-  //   const { AdvancedMarkerView } = await google.maps.importLibrary("marker");
+  mobile_are.forEach((obj) => {
+    obj.addEventListener('click', function(e){
+      this.parentNode.classList.add('active');
+      console.log(this)
+      this.querySelector('ul').classList.add('active')
+    })
+  })
   
-  //   // The map, centered at Uluru
-  //   map = new Map(document.getElementById("map"), {
-  //     zoom: 4,
-  //     center: position,
-  //     mapId: "DEMO_MAP_ID",
-  //   });
-  
-  //   // The marker, positioned at Uluru
-  //   const marker = new AdvancedMarkerView({
-  //     map: map,
-  //     position: position,
-  //     title: "Uluru",
-  //   });
-  // }
-  
-  // initMap();
 });
